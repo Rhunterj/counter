a3072d639e602c5b20ea744c774ff43500d1e07e
Rhunterj <robbin_jagt@hotmail.com>
2018-12-06 23:09:35 +0100

    Changed title

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Navbar extends Component {
     render() { 
         return ( <nav className="navbar navbar-light bg-light">
-        <a className="navbar-brand" href="#">Navbar</a>
+        <a className="navbar-brand" href="#">Counter</a>
       </nav>
        );
     }
